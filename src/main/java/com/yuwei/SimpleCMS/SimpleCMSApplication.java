cc5b3efb16339a0ddc06261f33352fc213583353
Danica8251 <103239260+Danica8251@users.noreply.github.com>
2023-03-17 17:49:15 +0800

    新增java基本設定

---
+++ src/main/java/com/yuwei/SimpleCMS/SimpleCMSApplication.java
@@ -0,0 +1,13 @@
+package com.yuwei.SimpleCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCMSApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/yuwei/SimpleCMS/config/CorsConfig.java
@@ -0,0 +1,49 @@
+package com.yuwei.SimpleCMS.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    // 當前跨域請求最大有效時長。這裡默認1天
+//    private static final long MAX_AGE = 24 * 60 * 60;
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+////        corsConfiguration.addAllowedOrigin("http://localhost:8081"); // 1 設置訪問源位址
+//        corsConfiguration.addAllowedOrigin("*"); // 1 設置訪問源位址
+//        corsConfiguration.addAllowedHeader("*"); // 2 設置訪問源請求頭
+//        corsConfiguration.addAllowedMethod("*"); // 3 設置訪問源請求方法
+//        corsConfiguration.setMaxAge(MAX_AGE);
+//        source.registerCorsConfiguration("/**", corsConfiguration); // 4 對介面配置跨域設置
+//        return new CorsFilter(source);
+//    }
+//}
+//
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 解决跨域問題
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
